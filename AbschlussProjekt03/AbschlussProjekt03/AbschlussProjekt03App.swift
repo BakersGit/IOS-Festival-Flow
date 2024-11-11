04d2c17f9b2bfe942b61cc3fdd8881bc38b84df9
Kai Becker <KBecker92@web.de>
2024-11-11 12:45:54 +0100

    no message

---
+++ AbschlussProjekt03/AbschlussProjekt03/AbschlussProjekt03App.swift
@@ -0,0 +1,17 @@
+//
+//  AbschlussProjekt03App.swift
+//  AbschlussProjekt03
+//
+//  Created by Kai Becker on 11.11.24.
+//
+
+import SwiftUI
+
+@main
+struct AbschlussProjekt03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AbschlussProjekt03/AbschlussProjekt03/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AbschlussProjekt03
+//
+//  Created by Kai Becker on 11.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
