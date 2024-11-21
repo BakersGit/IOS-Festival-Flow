31ffa966d4d76bb3e4ed9c156c3ff423a744f4ea
Kai Becker <KBecker92@web.de>
2024-11-21 17:27:53 +0100

    MVVM-Struktur aufgebaut

---
+++ AbschlussProjekt03/AbschlussProjekt03/Views/Settings/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  AbschlussProjekt03
+//
+//  Created by Kai Becker on 21.11.24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
